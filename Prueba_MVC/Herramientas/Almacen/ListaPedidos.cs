7e81d1b05e5d7d76c03146e8cc3ae1f6ee6a0d10
VelizAndres <dandresveliz@gmail.com>
2020-03-04 10:36:31 -0600

    Vistas de Pedido

---
+++ Prueba_MVC/Herramientas/Almacen/ListaPedidos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Prueba_MVC.Models;
+namespace Prueba_MVC.Herramientas.Almacen
+{
+    public class ListaPedidos
+    {
+        private static ListaPedidos _instance = null;
+
+        public static ListaPedidos Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new ListaPedidos();
+                return _instance;
+            }
+        }
+
+        public LinkedList<mPedido> arbolFarm = new LinkedList<mPedido>();
+    }
+}
---
+++ Prueba_MVC/Models/mCompraFarmaco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prueba_MVC.Models
+{
+    public class mCompraFarmaco
+    {
+        private string nombre;
+        private int cantidad;
+        private double precio_uni;
+        private double subtotal;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Cantidad { get => cantidad; set => cantidad = value; }
+        public double Precio_uni { get => precio_uni; set => precio_uni = value; }
+        public double Subtotal { get => subtotal; set => subtotal = value; }
+    }
+}
